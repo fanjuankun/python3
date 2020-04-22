a19d763ba423f9fad4ba0bf9e5d533e238b8704f
fanjuankun <fanjuankun@outlook.com>
2020-04-22 19:20:43 +0800

    学生信息查看

---
+++ student.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    struct Student
+    {
+        char name[20];
+        int age;
+        int num;
+        char Class;
+        int score;
+    }
+    struct Student stu[10];
+    int a;
+    printf("请输入学生信息：\n");
+    
+}
