2e266af54f1218fc5684d35b12c73a02132f90e4
fanjuankun <fanjuankun@outlook.com>
2020-05-13 20:22:16 +0800

    格式化输出

--- test03.py
+++ test03.py
@@ -1,1 +1,11 @@
-#今年我的年龄为x岁
+Age =18
+Name='TOM'
+Weight=75.5
+Stu_Id=1
+#1、今年我的年龄为x岁
+print('今年我的年龄为%d岁' %Age)
+print('我的名字是%s' %Name)
+print('我的体重是%.1f公斤' %Weight)
+print('我的学号是%d' %Stu_Id)
+print('我的名字是%s，今年%d岁了，体重%.1f公斤，学号是%d' %(Name,Age,Weight,Stu_Id))
+print(f'我的名字是{Name}，今年{Age}岁了，体重{Weight}公斤，学号是{Stu_Id}')
